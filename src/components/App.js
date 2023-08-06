02f15c731d046c2a23b87378a0295086c20355c7
Sandeep <newsandeep03@gmail.com>
2023-08-06 19:12:41 +0530

    third time ans

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-import Form from "./Form";
 
 const App = () => {
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: ""
+  })
+
+  const printInfo = (e) => {
+    e.preventDefault();
+    console.log(form);
+  }
+
   return (
     <div>
-        <Form />
+        <form onSubmit={printInfo}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" onChange={(e) => setForm({...form, name: e.target.value})} value={form.name}
+          />
+          <br />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" onChange={(e) => setForm({...form, email: e.target.value})} value={form.email}
+          />
+          <br />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" onChange={(e) => setForm({...form, password: e.target.value})}
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
     </div>
   )
 }
